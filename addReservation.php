9c0db14d47506f3e138caa56ade17c3127f5040f
Martin Chanudet <machanudet@stpbb.org>
2023-04-14 16:43:00 +0200

    correction requète

--- addReservation.php
+++ addReservation.php
@@ -1,33 +1,42 @@
 <?php
+session_start();
     require_once "connection.php";
 
-    $sqlPersonne=$connection ->prepare('SELECT nomUser, psw FROM utilisateur WHERE nomUser = :user');
+    var_dump($_SESSION);
+
+    $sqlPersonne=$connection ->prepare('SELECT idUser FROM utilisateur WHERE nomUser = :user');
     $sqlPersonne->bindParam(":user", $_SESSION['username']);
 
     $sqlPersonne->execute();
+
+    $sqlPersonne->debugDumpParams();
 
     $lignePersonne = $sqlPersonne->fetchall();
     $idUser;
     foreach($lignePersonne as $personne){
         $idUser= $personne['idUser'];
     }
+    echo"<br><br><br>";
 
+    //Vérifier si la date n'a pas déjà été prise par une autre réservation
 
-    $sqlReservation=$connection ->prepare('INSERT INTO reservation(\'dateDebut\',\'dateFin\',\'idUser\',\'idVehicule\') VALUES (:dateDebut,:dateFin,:idUser,:idVehicule,)');
+    $sqlReservation=$connection ->prepare('INSERT INTO reservation(dateDebut, dateFin, idUser, idVehicule) VALUES (:dateDebut, :dateFin, :idUser, :idVehicule);');
 
     $sqlReservation->bindParam(":dateDebut", $_REQUEST['dateDebut']);
     $sqlReservation->bindParam(":dateFin", $_REQUEST['dateFin']);
     $sqlReservation->bindParam(":idUser", $idUser);
     $sqlReservation->bindParam(":idVehicule", $_REQUEST['idVehicule']);
 
-
-
     $sqlReservation->execute();
-    $ligneReservation = $sqlReservation->fetchall();
-
-
-    foreach($ligneReservation as $reservation){
+    $sqlReservation->debugDumpParams();
+    
+    if($sqlReservation){
+        echo"ajouté";
+    }else{
+        echo"erreur";
     }
+
+
 
 
 
