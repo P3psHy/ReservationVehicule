0facfc49a1c8440939aec5fe24f130d94270c1a3
Martin Chanudet <machanudet@stpbb.org>
2023-04-14 14:33:10 +0200

    ajouts fichiers

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reservation Véhicule</title>
+</head>
+<body>
+    <a href="ajouterReservation.php"><button>Ajouter Reservation</button></a>
+</body>
+</html>
